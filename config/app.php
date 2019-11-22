d9420c9851abdb1364f332928a481fc65f8b27ba
liudandan <1064265199@qq.com>
2019-11-22 22:56:05 +0800

    添加laravel-admin

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -219,7 +219,8 @@ return [
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
+//        'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Schema' => Jialeo\LaravelSchemaExtend\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
